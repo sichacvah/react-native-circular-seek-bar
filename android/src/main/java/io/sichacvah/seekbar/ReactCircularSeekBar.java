a5a7734f90e77f53e6446eece45f28474ab82043
Sichacvah <sichirc@gmail.com>
2016-11-28 14:57:15 +0500

    fix

--- android/src/main/java/io/sichacvah/seekbar/ReactCircularSeekBar.java
+++ android/src/main/java/io/sichacvah/seekbar/ReactCircularSeekBar.java
@@ -367,37 +367,37 @@ public class ReactCircularSeekBar extends View {
 	 * @param attrArray TypedArray containing the attributes.
 	 */
 	protected void initAttributes(TypedArray attrArray) {
-		mCircleXRadius = attrArray.getDimension(R.styleable.CircularSeekBar_circle_x_radius, DEFAULT_CIRCLE_X_RADIUS * DPTOPX_SCALE);
-		mCircleYRadius = attrArray.getDimension(R.styleable.CircularSeekBar_circle_y_radius, DEFAULT_CIRCLE_Y_RADIUS * DPTOPX_SCALE);
-		mPointerRadius = attrArray.getDimension(R.styleable.CircularSeekBar_pointer_radius, DEFAULT_POINTER_RADIUS * DPTOPX_SCALE);
-		mPointerHaloWidth = attrArray.getDimension(R.styleable.CircularSeekBar_pointer_halo_width, DEFAULT_POINTER_HALO_WIDTH * DPTOPX_SCALE);
-		mPointerHaloBorderWidth = attrArray.getDimension(R.styleable.CircularSeekBar_pointer_halo_border_width, DEFAULT_POINTER_HALO_BORDER_WIDTH * DPTOPX_SCALE);
-		mCircleStrokeWidth = attrArray.getDimension(R.styleable.CircularSeekBar_circle_stroke_width, DEFAULT_CIRCLE_STROKE_WIDTH * DPTOPX_SCALE);
+		mCircleXRadius = DEFAULT_CIRCLE_X_RADIUS * DPTOPX_SCALE;
+		mCircleYRadius = DEFAULT_CIRCLE_Y_RADIUS * DPTOPX_SCALE;
+		mPointerRadius = DEFAULT_POINTER_RADIUS * DPTOPX_SCALE;
+		mPointerHaloWidth = DEFAULT_POINTER_HALO_WIDTH * DPTOPX_SCALE;
+		mPointerHaloBorderWidth = DEFAULT_POINTER_HALO_BORDER_WIDTH * DPTOPX_SCALE;
+		mCircleStrokeWidth = DEFAULT_CIRCLE_STROKE_WIDTH * DPTOPX_SCALE;
 
-		mPointerColor = attrArray.getColor(R.styleable.CircularSeekBar_pointer_color, DEFAULT_POINTER_COLOR);
-		mPointerHaloColor = attrArray.getColor(R.styleable.CircularSeekBar_pointer_halo_color, DEFAULT_POINTER_HALO_COLOR);
-		mPointerHaloColorOnTouch = attrArray.getColor(R.styleable.CircularSeekBar_pointer_halo_color_ontouch, DEFAULT_POINTER_HALO_COLOR_ONTOUCH);
-		mCircleColor = attrArray.getColor(R.styleable.CircularSeekBar_circle_color, DEFAULT_CIRCLE_COLOR);
-		mCircleProgressColor = attrArray.getColor(R.styleable.CircularSeekBar_circle_progress_color, DEFAULT_CIRCLE_PROGRESS_COLOR);
-		mCircleFillColor = attrArray.getColor(R.styleable.CircularSeekBar_circle_fill, DEFAULT_CIRCLE_FILL_COLOR);
+		mPointerColor = DEFAULT_POINTER_COLOR;
+		mPointerHaloColor = DEFAULT_POINTER_HALO_COLOR;
+		mPointerHaloColorOnTouch = DEFAULT_POINTER_HALO_COLOR_ONTOUCH;
+		mCircleColor = DEFAULT_CIRCLE_COLOR;
+		mCircleProgressColor = DEFAULT_CIRCLE_PROGRESS_COLOR;
+		mCircleFillColor = DEFAULT_CIRCLE_FILL_COLOR;
 
 		mPointerAlpha = Color.alpha(mPointerHaloColor);
 
-		mPointerAlphaOnTouch = attrArray.getInt(R.styleable.CircularSeekBar_pointer_alpha_ontouch, DEFAULT_POINTER_ALPHA_ONTOUCH);
+		mPointerAlphaOnTouch = DEFAULT_POINTER_ALPHA_ONTOUCH;
 		if (mPointerAlphaOnTouch > 255 || mPointerAlphaOnTouch < 0) {
 			mPointerAlphaOnTouch = DEFAULT_POINTER_ALPHA_ONTOUCH;
 		}
 
-		mMax = attrArray.getInt(R.styleable.CircularSeekBar_max, DEFAULT_MAX);
-		mProgress = attrArray.getInt(R.styleable.CircularSeekBar_progress, DEFAULT_PROGRESS);
-		mCustomRadii = attrArray.getBoolean(R.styleable.CircularSeekBar_use_custom_radii, DEFAULT_USE_CUSTOM_RADII);
-		mMaintainEqualCircle = attrArray.getBoolean(R.styleable.CircularSeekBar_maintain_equal_circle, DEFAULT_MAINTAIN_EQUAL_CIRCLE);
-		mMoveOutsideCircle = attrArray.getBoolean(R.styleable.CircularSeekBar_move_outside_circle, DEFAULT_MOVE_OUTSIDE_CIRCLE);
-		lockEnabled = attrArray.getBoolean(R.styleable.CircularSeekBar_lock_enabled, DEFAULT_LOCK_ENABLED);
+		mMax = DEFAULT_MAX;
+		mProgress = DEFAULT_PROGRESS;
+		mCustomRadii = DEFAULT_USE_CUSTOM_RADII;
+		mMaintainEqualCircle = DEFAULT_MAINTAIN_EQUAL_CIRCLE;
+		mMoveOutsideCircle = DEFAULT_MOVE_OUTSIDE_CIRCLE;
+		lockEnabled = DEFAULT_LOCK_ENABLED;
 
 		// Modulo 360 right now to avoid constant conversion
-		mStartAngle = ((360f + (attrArray.getFloat((R.styleable.CircularSeekBar_start_angle), DEFAULT_START_ANGLE) % 360f)) % 360f);
-		mEndAngle = ((360f + (attrArray.getFloat((R.styleable.CircularSeekBar_end_angle), DEFAULT_END_ANGLE) % 360f)) % 360f);
+		mStartAngle = ((360f + DEFAULT_START_ANGLE % 360f)) % 360f);
+		mEndAngle = ((360f + DEFAULT_END_ANGLE % 360f)) % 360f);
 
 		if (mStartAngle == mEndAngle) {
 			//mStartAngle = mStartAngle + 1f;
@@ -820,7 +820,7 @@ public class ReactCircularSeekBar extends View {
 	}
 
 	protected void init(AttributeSet attrs, int defStyle) {
-		final TypedArray attrArray = getContext().obtainStyledAttributes(attrs, R.styleable.CircularSeekBar, defStyle, 0);
+		final TypedArray attrArray = getContext().obtainStyledAttributes(attrs, "CircularSeekBar", defStyle, 0);
 
 		initAttributes(attrArray);
 
