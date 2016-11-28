75132f168bc46650f0c14b266531bd0f9c439770
Sichacvah <sichirc@gmail.com>
2016-11-28 15:51:38 +0500

    change ViewManager to SimpleViewManager

--- android/src/main/java/io/sichacvah/seekbar/ReactCircularSeekBarPackage.java
+++ android/src/main/java/io/sichacvah/seekbar/ReactCircularSeekBarPackage.java
@@ -7,7 +7,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.uimanager.ViewManager;
+import com.facebook.react.uimanager.SimpleViewManager;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -36,8 +36,8 @@ public class ReactCircularSeekBarPackage implements ReactPackage {
     }
 
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Arrays.<ViewManager>asList(
+    public List<SimpleViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<VieSimpleViewManagerwManager>asList(
             new ReactCircularSeekBarManager()
         );
         
